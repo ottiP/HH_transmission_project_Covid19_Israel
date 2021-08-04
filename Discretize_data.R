423a58862beda1d25ec8663ab59bd3390d240006
weinbergerlab <dweinber@gmail.com>
2021-08-04 17:09:27 -0400

    hh txn piece

--- Discretize_data.R
+++ Discretize_data.R
@@ -48,4 +48,46 @@ d1$agegrp[d1$AGE>=60 & d1$age<150] <- 3
 #Step 2, Count exposure days in the household based on the number of infected people in each group
 ####################################################################################################################
     
+    #NOTE: need to filter out HH with no infections
     
+    #For each HH that has an infection, need to define for each day how many contacts are infected
+    
+    #What is the first date of infection in each household
+    d1 <- d1 %>%
+      group_by(HH_CERTAIN) %>%
+      mutate(
+        hh.first.date = min(exposed.date, na.rm = T),
+        n.infections.hh = sum(infected, na.rm = T),
+        n.people.hh = length(infected),
+        
+      ) %>%
+      arrange(HH_CERTAIN)
+    
+    #FIlter out HH with at least 1 infections and 2+ people for this calculation since focus is on HH transmission specifically
+    b1 <- d1[d1$n.infections.hh>0 & d1$n.people.hh>=2 ,]
+    
+ 
+    #could vectorize this whole thing instead of loop
+   # for(d in 0:40){
+    for(d in 3){
+    
+      b1$cal_date <- b1$hh.first.date + d #what is the current date in the HH (defined relative to first infection in HH)
+      
+      b1$vax <- ((b1$vax2dose_date + 10) < b1$cal_date )#was the person vaccinated 10+ days before the current date?
+      
+      b1$not_infected <- b1$exposed.date > b1$cal_date  # has the person been infected by cal_date?
+      
+      b1$infectious <- (b1$cal_date >= b1$infect.date) & (b1$cal_date < b1$end.infectious.date) #is the individual infectious at this time?
+    
+      c1 <- b1[,c("HH_CERTAIN",'agegrp', 'vax', 'not_infected', 'infectious')]
+    
+      #(Next step--copy c1, change names, then do man-many merge by HH with c1)
+    }
+    
+    
+    
+    n.hh.days <- 0:40 #from day 0-40 of follow up in the household
+    n.day.hh <- lapply(n.hh.days , function(x){
+      
+      
+    })
