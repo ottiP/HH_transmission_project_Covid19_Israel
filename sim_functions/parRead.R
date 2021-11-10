cb559cebaf45d71fd934cfd48b023d05940d5f45
ottiP <ottavia.prunas@edu.unito.it>
2021-11-10 11:07:52 -0500

    minor fix

--- sim_functions/parRead.R
+++ sim_functions/parRead.R
@@ -1,5 +1,5 @@
 parRead <- function(N){
-  summary_tables <- readRDS(paste('../intermediate_data/summary_table_',N,'iter_SIM.rds'))
+  summary_tables <- readRDS(paste('./intermediate_data/summary_table_',N,'iter_SIM.rds'))
   output <- model.run(summary_tables)
   return(output)
 }
